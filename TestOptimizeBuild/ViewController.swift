dc296006cd2269815c0d1c48378a65f88cf46a1e
Ilia Biltuev <ilia.biltuev@ukg.com>
2023-05-15 14:39:32 +0300

    spm updates

--- TestOptimizeBuild/ViewController.swift
+++ TestOptimizeBuild/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import Kingfisher
+import Starscream
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Kingfisher
+        let imageView = UIImageView(frame: .init(origin: .zero, size: CGSize(width: 200, height: 200)))
+        let url = URL(string: "https://example.com/image.png")
+        imageView.kf.setImage(with: url)
+
+        // Starscream
+        var request = URLRequest(url: URL(string: "http://localhost:8080")!)
+        request.timeoutInterval = 5
+        let socket = WebSocket(request: request)
+        socket.connect()
     }
 
     // TODO:
